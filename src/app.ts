e35750e0f3c5632757eb9e552258202b7c928b10
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2024-12-11 14:15:52 -0400

    [Websocket] Broadcast from websocket server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log("Hola mundo");
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on("connection", function connection(ws) {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    //envia la data al cliente de regreso
+
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString(),
+    });
+
+    // ws.send(JSON.stringify(payload));
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
+  });
+
+  //   ws.send("Hola desde el servidor");
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("Server running on port 3000");
